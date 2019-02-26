3dceac936e49c8463fb4e3bd934f6f5ccdec75dc
dhanush1811 <dhanush18113251@gmail.com>
2019-02-26 22:22:55 +0530

    Create userinput_server_name.py

---
+++ userinput_server_name.py
@@ -0,0 +1,14 @@
+def string():
+    print("Enter server name:")
+    server_name=input()
+    print("Enter the DB name:")
+    db_name=input()
+    print("enter username:")
+    user_name=input()
+    print("enter the password:")
+    password=input()
+    
+    return "Server_Name="+server_name+";DB_Name="+db_name+";Username="+user_name+";Password="+password
+
+l=string()
+print(l)
